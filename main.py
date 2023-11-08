f40ebabadc412cbd09a824e3d3715d12daace2ea
mclaramarinho <marinho.claramb@gmail.com>
2023-11-08 19:50:08 -0300

    yay

---
+++ main.py
@@ -0,0 +1,10 @@
+from get_data import get_data
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/veiculos')
+def exec():
+    name = request.args.get("name")
+    return get_data(name)
